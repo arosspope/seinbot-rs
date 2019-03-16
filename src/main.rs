7aa0719aaaa64335e65aac9e8f244c1fedea634e
arosspope <andrew.pope456@gmail.com>
2019-03-16 23:05:51 +1100

    parsed actor files

--- src/main.rs
+++ src/main.rs
@@ -6,33 +6,38 @@ use std::path::{PathBuf};
 use markov::Chain;
 use glob::glob;
 
-use rand::thread_rng;
+use rand::{thread_rng, Rng};
 use rand::seq::SliceRandom;
 
 
 fn main() {
     let actor_file = choose_actor();
-    let actor = actor_file.display().to_string().replace("corpus/", "").replace(".txt", "");
+    let actor = actor_file.display().to_string().replace("actors/", "").replace(".txt", "");
     
+    print!("[{}] ", actor);
     for s in markov_gen(actor_file) {
-        
-        println!("[{}] {}",
-            actor,
-            s
-        );
+        print!("{} ", capitalise_sentence(&s));
+    }
+    println!("")
+}
+
+fn capitalise_sentence(s: &str) -> String {
+    let mut c = s.chars();
+    match c.next() {
+        None => String::new(),
+        Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
     }
 }
 
+
 fn choose_actor() -> PathBuf {
-    let files: Vec<PathBuf> = glob("corpus/*.txt").unwrap().filter_map(Result::ok).collect();
+    let files: Vec<PathBuf> = glob("actors/*.txt").unwrap().filter_map(Result::ok).collect();
     let mut rng = thread_rng();
     let actor = files.choose(&mut rng).expect("the stage is not set");
     actor.to_owned()
 }
 
-fn markov_gen(actor: PathBuf) -> Vec<String>{
-    let count = 1;
-    
+fn markov_gen(script: PathBuf) -> Vec<String> {
     // Lower order: more random crazy stuff
     // Higher order: more likely to match original line (will need to strip text of punctuation - sentences on new lines)
     
@@ -40,8 +45,8 @@ fn markov_gen(actor: PathBuf) -> Vec<String>{
     // TODO: Strip opening/closing brackets that aren't a pair
     
     let mut chain = Chain::of_order(2);
-    chain.feed_file(actor).unwrap();
-    if chain.is_empty() { panic!("No files were fed into the chain.") }
+    chain.feed_file(script).unwrap();
     
-    chain.str_iter_for(count).collect()
+    let mut rng = thread_rng();
+    chain.str_iter_for(rng.gen_range(1, 5)).map(|s| s.replace("</font>", "")).collect()
 }
