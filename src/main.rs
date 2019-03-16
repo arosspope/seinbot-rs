ade73a59da92b9e7d3fb05e73c19d33d23ac9c6d
arosspope <andrew.pope456@gmail.com>
2019-03-16 21:03:05 +1100

    basic text generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate markov;
+
+use std::path::Path;
+use markov::Chain;
+
 fn main() {
-    println!("Hello, world!");
+    for s in markov_gen() {
+        println!("{:?}", s);
+    }
+}
+
+fn markov_gen() -> Vec<String>{
+    let count = 1;
+    
+    // let mut chain = Chain::new();
+    let mut chain = Chain::of_order(4);
+    chain.feed_file(Path::new("corpus/jerry.txt")).unwrap();
+    if chain.is_empty() { panic!("No files were fed into the chain.") }
+    
+    chain.str_iter_for(count).collect()
 }
