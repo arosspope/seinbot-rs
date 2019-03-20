ab6aafb6b44276b12b5f19ccdfee1b96ee51c670
arosspope <andrew.pope456@gmail.com>
2019-03-20 19:07:58 +1100

    falling back on environment variables

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,5 @@
+use serde_derive::{Deserialize, Serialize};
+use std::env;
 use std::fs::File;
 use std::path::Path;
 
@@ -10,11 +12,45 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn read(path_file: &Path) -> Option<Config> {
-        let mut file = match File::open(path_file) {
-            Ok(f) => f,
-            Err(_) => return None,
+    pub fn read(path_file: &Path) -> Config {
+        let file = File::open(path_file);
+
+        if file.is_err() {
+            return Config::from_env(); // Try loading from env as a fallback
+        }
+
+        serde_json::from_reader(&mut file.unwrap())
+            .ok()
+            .unwrap_or_else(Config::from_env) // Again as a fallback
+    }
+
+    /// This should only be called as a fallback
+    pub fn from_env() -> Config {
+        let consumer_key = match env::var("SEINBOT_CONSUMER_KEY") {
+            Ok(val) => val,
+            Err(_) => panic!("couldn't find consumer key"),
         };
-        serde_json::from_reader(&mut file).ok()
+
+        let consumer_secret = match env::var("SEINBOT_CONSUMER_SECRET") {
+            Ok(val) => val,
+            Err(_) => panic!("couldn't find consumer secret"),
+        };
+
+        let access_key = match env::var("SEINBOT_ACCESS_KEY") {
+            Ok(val) => val,
+            Err(_) => panic!("couldn't find access key"),
+        };
+
+        let access_secret = match env::var("SEINBOT_ACCESS_SECRET") {
+            Ok(val) => val,
+            Err(_) => panic!("couldn't find access secret"),
+        };
+
+        Config {
+            consumer_key,
+            consumer_secret,
+            access_key,
+            access_secret,
+        }
     }
 }
